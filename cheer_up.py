fdc1168c8cf355e6cca65bd28d8d2125e4cdcd11
Bradley Johnson <bradley.johnson.555@gmail.com>
2018-07-30 13:17:25 -0700

    move kittenList to new file

--- cheer_up.py
+++ cheer_up.py
@@ -14,19 +14,10 @@ def botLogin():
 			user_agent = "cheer_up_bot")
 	return r
 
-#if changing kittenList, don't forget to adjust kittenListSize
-kittenList = ['http://freshpet.com/wp-content/uploads/2015/04/kitten.jpg', 
-'http://upshout.net/wp-content/uploads/2015/06/dwarf-kitten-01.jpg', 'https://assets3.thrillist.com/v1/image/2563373/size/tmg-article_tall;jpeg_quality=20.jpg',
-'https://img.buzzfeed.com/buzzfeed-static/static/2014-11/4/15/enhanced/webdr03/longform-original-29174-1415133936-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
-'https://www.vets4pets.com/_resources/assets/inline/full/0/236990.jpg', 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
-'https://i.redd.it/x3kdiz7u9u2z.jpg', 'https://i.reddituploads.com/586cc251d1b94dbf9a685161b911eadf?fit=max&h=1536&w=1536&s=c4bad307b5767dde1b391fcc82cd126a',
-'https://i.redd.it/yr01t7xz5rwy.jpg', 'https://i.redd.it/2af88hhk9dsy.jpg', 'https://i.redd.it/0tx76hl4v5ty.jpg']
-kittenListSize = 10
-
 def reply(comment, str): # str = trigger
 	try:
 		if comment.author.name != "cheer_up_bot" and comment.subreddit.display_name.lower() not in BLACKLIST:
-			kittenPic = kittenList[random.randint(0, kittenListSize)]
+			kittenPic = kitten_pics.kittenList[random.randint(0, kitten_pics.kittenListSize)]
 			comment.reply('>' + str +"\n\n[Here is a picture of a kitten to cheer you up](" + kittenPic + ")")
 			print ("\nsad person found -> " + comment.body)
 			time.sleep(3)
---
+++ kitten_pics.py
@@ -0,0 +1,14 @@
+
+# if updating list, don't forget to update kittenListSize
+kittenList = ['http://freshpet.com/wp-content/uploads/2015/04/kitten.jpg',
+'http://upshout.net/wp-content/uploads/2015/06/dwarf-kitten-01.jpg',
+ 'https://assets3.thrillist.com/v1/image/2563373/size/tmg-article_tall;jpeg_quality=20.jpg',
+'https://img.buzzfeed.com/buzzfeed-static/static/2014-11/4/15/enhanced/webdr03/longform-original-29174-1415133936-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
+'https://www.vets4pets.com/_resources/assets/inline/full/0/236990.jpg',
+ 'http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg',
+'https://i.redd.it/x3kdiz7u9u2z.jpg',
+'https://i.reddituploads.com/586cc251d1b94dbf9a685161b911eadf?fit=max&h=1536&w=1536&s=c4bad307b5767dde1b391fcc82cd126a',
+'https://i.redd.it/yr01t7xz5rwy.jpg',
+'https://i.redd.it/2af88hhk9dsy.jpg',
+'https://i.redd.it/0tx76hl4v5ty.jpg']
+kittenListSize = 10
